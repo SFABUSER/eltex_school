f9a559695eff7dc45662191a0146d8f87f6bcf28
nfoman <nfoman@list.ru>
2024-09-25 20:56:22 +0700

    some fixes

--- Part1/Task_16/lab3/multicastServer.c
+++ Part1/Task_16/lab3/multicastServer.c
@@ -15,15 +15,12 @@ struct sockaddr_in client;
 
 void main()
 {
-    int flag = 1;
-
     client.sin_family = AF_INET;
     client.sin_port = PORT;
     inet_pton(AF_INET, "224.0.0.1", &client.sin_addr);
     socklen_t client_size = sizeof(client);
     
     int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    setsockopt(sock_fd, SOL_SOCKET, SO_BROADCAST, &flag, sizeof(flag));
 
     sendto(sock_fd, "Multicast message!", MSG_SIZE, 0, (struct sockaddr *)&client, client_size);
 
